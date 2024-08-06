e8b3085de2835f11fc8870a4e4df7f989df83e07
zzq89757 <1604653835@qq.com>
2024-08-06 17:23:46 +0800

    cof header

---
+++ src/bin/fold_pre_process/cof.h
@@ -0,0 +1,18 @@
+
+#define PUBLIC
+#define PRIVATE static
+#define K0  273.15
+/** Infinity as used in minimization routines */
+#define INF 10000000 /* (INT_MAX/10) */
+
+#define EMAX (INF/10)
+/** forbidden */
+#define FORBIDDEN 9999
+/** bonus contribution */
+#define BONUS 10000
+/** The number of distinguishable base pairs */
+#define NBPAIRS 7
+/** The minimum loop length */
+#define TURN 3
+/** The maximum loop length */
+#define MAXLOOP 30
