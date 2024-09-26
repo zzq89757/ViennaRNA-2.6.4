7b124ec2f3f58798022c9a5080593f61dffb161f
zzq89757 <1604653835@qq.com>
2024-09-26 09:05:13 +0800

    run cofold opt

--- src/bin/run_cofold.sh
+++ src/bin/run_cofold.sh
@@ -1,7 +1,7 @@
 # cd ../ && make -s > make.log&& cd bin && ./RNAcofold -d 2 -p --noPS --noGU --noconv --noClosingGU -P /data/ntc/Repository/ViennaRNA-2.6.4_make/misc/dna_mathews2004.par < fold_test.seq > cofold.out
 #  ./RNAcofold -d 2 -p --noPS --noGU --noconv --noClosingGU  < fold_test.seq >> cofold.out
  ./RNAcofold -a -d 0 -p --noPS --noGU --noconv --noClosingGU  --noLP < fold_test.seq > cofold.out
- ./RNAcofold -a -d 0 -p --noPS --noGU --noconv --noClosingGU  -P /data/ntc/Repository/ViennaRNA-2.6.4_make/misc/dna_mathews2004.par< fold_test.seq >> cofold.out
+ ./RNAcofold -a -d 0 -p --noPS --noGU --noconv --noClosingGU  -P /data/ntc/Repository/ViennaRNA-2.6.4_make/misc/dna_mathews1999.par< fold_test.seq >> cofold.out
 RNAcofold -a -d 0  --noPS --noGU  --noClosingGU  --noLP --noconv< fold_test.seq >> cofold.out
 RNAcofold -a -d 0  --noPS --noGU  --noClosingGU  --noLP -P /data/ntc/Repository/ViennaRNA-2.6.4_make/misc/dna_mathews2004.par  --noconv< fold_test.seq >> cofold.out
 # python cofold.py
